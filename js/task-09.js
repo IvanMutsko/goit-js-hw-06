5f3bff28dedd022686980634c364252704fff75d
Ivan Mutsko <imucko1996@gmail.com>
2023-01-30 21:29:42 +0200

    complite task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+// генератор кольорів
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// пошук елементів сторінки
+const colorBtn = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+const body = document.querySelector('body');
+
+// слухач на клік по кнопці
+colorBtn.addEventListener('click', event => {
+  // виклик функції генератора кольору
+  const newColor = getRandomHexColor();
+  // запис значення кольору в спан
+  colorValue.textContent = newColor;
+  // заміна бекграунду
+  body.style.backgroundColor = newColor;
+});
